312e12f2168bb0df3d455e0a6801795a3ef98d60
rdchen1124 <rdchen1124@gmail.com>
2018-10-24 11:31:45 +0800

    multiples of 3 or 5 below 1000

---
+++ part_1_multiple.py
@@ -0,0 +1,6 @@
+# If we list all the natural numbers below 10 that are multiples of 3 or 5,
+# we get 3, 5, 6 and 9. The sum of these multiples is 23.
+match_lsit = [i for i in range(1,1000) if (i%3==0) or (i%5==0)]
+# print(match_lsit)
+num_sum = sum(match_lsit)
+print(num_sum)
